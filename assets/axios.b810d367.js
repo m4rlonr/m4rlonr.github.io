012b62e01169c2a10cc6565f92064119728c10b2
m4rlonr <marlon.rodrigues14@outlook.com>
2024-03-14 14:47:17 -0400

    v2.0

--- assets/axios.b810d367.js
+++
@@ -1,1 +0,0 @@
-import{b as e,k as a}from"./index.81acc850.js";const i=a.create({baseURL:"https://api.example.com"});var t=e(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=i});export{i as api,t as default};
