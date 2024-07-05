eb6f525f2c28bb96d806f72a0610a57d70b53d4c
unknown <marlon.rodrigues14@outlook.com>
2024-07-05 12:37:46 -0400

    new version

--- assets/FuncTools.3e84dcd4.js
+++
@@ -1,1 +0,0 @@
-function e(){return!!(navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/BlackBerry/i)||navigator.userAgent.match(/Windows Phone/i))}export{e as d};
